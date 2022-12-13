126ed2561b09fa41ef22c698101f64f9d910a329
Siying Zhao <2548015541@qq.com>
2022-12-13 21:48:38 +0800

    rnc: add class definition
    as title
    
    Signed-off-by: Siying Zhao <2548015541@qq.com>

--- riscnn_sim.py
+++ riscnn_sim.py
@@ -21,49 +21,68 @@ def riscnn_flow_from(local_mem, shared_mem):
 # Empty Implementation in real hardware since PE flow directly to local memory
     local_mem[:] = shared_mem[:]
 
-class ExeBlockA(object):
-    def __init__(self, block_id) -> None:
+def riscnn_set_ldst_base(i, j):
+    pass
+
+class ExeBlock(object):
+    def __init__(self, A_global: np.ndarray, B_global: np.ndarray, C_global: np.ndarray) -> None:
+        self.predicate = 0
+        self.A_global = A_global
+        self.B_global = B_global
+        self.C_global = C_global
+
+class ExeBlockA(ExeBlock):
+    def __init__(self, block_id, env: ExeBlock, B_shared: np.ndarray) -> None:
+        super().__init__(env.A_global, env.B_global, env.C_global)
         self.block_id = block_id
+        self.B_shared = B_shared
     # Local Memory Declaration
         self.C_local = np.empty((Mc, Nr), dtype="float32")
         self.A_local = np.empty((Mc, Kc), dtype="float32")
         self.B_local = np.empty((Kc, Nr), dtype="float32")
 
-    def run(self, A_global: np.ndarray, B_global: np.ndarray, C_global: np.ndarray,
-                    B_shared: np.ndarray, i: int, j: int):
+    # i & j are Iterators(or placeholder)
+    def run(self, i: int, j: int):
+    # Set LD_BASE & ST_BASE according to i & j
+        riscnn_set_ldst_base(i, j)
+    # Set Sparse Vector
+        self.predicate = (j == 0)
     # Load Stage
-        if (j == 0):
-            riscnn_load(self.A_local[:], A_global[self.block_id * Mc : self.block_id * Mc + Mc, i * Kc : i * Kc + Kc])
-        riscnn_load(self.B_local[:], B_global[i * Kc : i * Kc + Kc, j * Nr : j * Nr + Nr])
-        riscnn_load(self.C_local[:], C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
+        if (self.predicate):
+            riscnn_load(self.A_local[:], self.A_global[self.block_id * Mc : self.block_id * Mc + Mc, i * Kc : i * Kc + Kc])
+        riscnn_load(self.B_local[:], self.B_global[i * Kc : i * Kc + Kc, j * Nr : j * Nr + Nr])
+        riscnn_load(self.C_local[:], self.C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
     # Cal Stage
         riscnn_mat_muladd(self.C_local, self.A_local, self.B_local)
     # Flow Stage
-        riscnn_flow_to(self.B_local, B_shared)
+        riscnn_flow_to(self.B_local, self.B_shared)
     # Store Stage
-        riscnn_store(self.C_local[:,:], C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
+        riscnn_store(self.C_local[:,:], self.C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
 
-class ExeBlockB(object):
-    def __init__(self, block_id) -> None:
+class ExeBlockB(ExeBlock):
+    def __init__(self, block_id, env: ExeBlock, B_shared: np.ndarray) -> None:
+        super().__init__(env.A_global, env.B_global, env.C_global)
         self.block_id = block_id
+        self.B_shared = B_shared
     # Local Memory Declaration
         self.C_local = np.empty((Mc, Nr), dtype="float32")
         self.A_local = np.empty((Mc, Kc), dtype="float32")
         self.B_local = np.empty((Kc, Nr), dtype="float32")
 
-    def run(self, A_global: np.ndarray, B_global: np.ndarray, C_global: np.ndarray,
-                    B_shared: np.ndarray, i: int, j: int):
-        # Load Stage
-        if (j == 0):
-            riscnn_load(self.A_local[:], A_global[self.block_id * Mc : self.block_id * Mc + Mc, i * Kc : i * Kc + Kc])
-        riscnn_flow_from(self.B_local[:], B_shared[:])
-        riscnn_load(self.C_local[:], C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
-        # Cal Stage
+    def run(self, i: int, j: int):
+    # Set Sparse Vector
+        self.predicate = (j == 0)
+    # Load Stage
+        if (self.predicate):
+            riscnn_load(self.A_local[:], self.A_global[self.block_id * Mc : self.block_id * Mc + Mc, i * Kc : i * Kc + Kc])
+        riscnn_flow_from(self.B_local[:], self.B_shared[:])
+        riscnn_load(self.C_local[:], self.C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
+    # Cal Stage
         riscnn_mat_muladd(self.C_local, self.A_local, self.B_local)
-        # Store Stage
-        riscnn_store(self.C_local[:,:], C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
+    # Store Stage
+        riscnn_store(self.C_local[:,:], self.C_global[self.block_id * Mc : self.block_id * Mc + Mc, j * Nr : j * Nr + Nr])
 
-def virtual_centric_leader():
+def compute_graph():
     dtype = "float32"
     a_np = np.random.rand(M, K).astype(dtype)
     b_np = np.random.rand(K, N).astype(dtype)
@@ -71,17 +90,18 @@ def virtual_centric_leader():
     c_np = np.empty((M, N), dtype="float32")
 
     # Compute Graph
+    env = ExeBlock(a_np, b_np, c_np)
     b_shared = np.empty((Kc, Nr), dtype= "float32")
-    ebA = ExeBlockA(0)
+    ebA = ExeBlockA(0, env, b_shared)
     ebBs = []
     for block_id in range(1, M // Mc):
-        ebBs.append(ExeBlockB(block_id))
+        ebBs.append(ExeBlockB(block_id, env, b_shared))
     for i_k in range(K // Kc):
         for i_n in range(N // Nr):
-            ebA.run(a_np, b_np, c_np, b_shared, i_k, i_n)
+            ebA.run(i_k, i_n)
             for ebB in ebBs:
-                ebB.run(a_np, b_np, c_np, b_shared, i_k, i_n)
+                ebB.run(i_k, i_n)
     np.testing.assert_allclose(c_np, c_tmm, rtol=1e-5)
     print("Success!")
 
-virtual_centric_leader()
+compute_graph()
